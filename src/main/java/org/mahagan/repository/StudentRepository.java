8344bdd85797bf1778fa7516e3572ae6bd808862
viju045 <vijaykumarmohite05@gmail.com>
2023-07-30 17:09:06 +0530

    Add an Information about the Annotation

---
+++ src/main/java/org/mahagan/repository/StudentRepository.java
@@ -0,0 +1,15 @@
+/**
+ * Created By VijayKumar Moohite
+ * Date : 7/30/2023
+ * Time : 3:54 PM
+ * Project : RESTAPI-Crud-Operation
+ **/
+package org.mahagan.repository;
+
+import org.mahagan.domain.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository/* class provide the mechanism for storage,retrieval,search.update and delete operation on objects */
+public interface StudentRepository extends JpaRepository<Student,Integer> {
+}
